ab430ee605a082d91729b433a4b394daf78f9329
Abdualeem Hosny <abdualeem.hosny@nagwa.com>
2024-06-12 13:44:09 +0200

    Add project files.

---
+++ Rate Limiter Use IP Address/Program.cs
@@ -0,0 +1,68 @@
+using Rate_Limiter_Use_IP_Address;
+using System.Threading.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+    options.AddPolicy("fixed", context =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: context.Connection.RemoteIpAddress?.ToString() ?? "unknown",
+            factory: partition => new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = 10,
+                Window = TimeSpan.FromSeconds(10)
+            }
+        )
+    );
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRateLimiter();
+
+app.UseAuthorization();
+
+//app.MapControllers();
+
+// Define endpoints with rate limiting
+app.MapGet("/weatherforecast", async (HttpContext context) =>
+{
+    var summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    {
+        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = summaries[Random.Shared.Next(summaries.Length)]
+    })
+    .ToArray();
+
+    await context.Response.WriteAsJsonAsync(forecasts);
+})
+.RequireRateLimiting("fixed"); 
+
+app.Run();
